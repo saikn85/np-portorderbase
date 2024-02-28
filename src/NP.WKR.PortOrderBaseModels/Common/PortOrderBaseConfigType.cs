76ef74ce3e2f74040ce923f9ff0d231cc1984e99
saikn85 <sai.nanduri14@gmail.com>
2024-02-29 00:16:29 +0530

    added project references, introduced enum

---
+++ src/NP.WKR.PortOrderBaseModels/Common/PortOrderBaseConfigType.cs
@@ -0,0 +1,27 @@
+﻿namespace NP.WKR.PortOrderBase.Models.Common;
+
+/// <summary>
+/// 
+/// </summary>
+public enum PortOrderBaseConfigType
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    Geographic,
+
+    /// <summary>
+    /// 
+    /// </summary>
+    NonGeographic,
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    ActivationExcel,
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    ActivationCsv
+}
--- src/NP.WKR.PortOrderBaseService/Ultities/PortOrderBaseConfigCache.cs
+++ src/NP.WKR.PortOrderBaseService/Ultities/PortOrderBaseConfigCache.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
+using NP.WKR.PortOrderBase.Models.Common;
 
 namespace NP.WKR.PortOrderBase.Service.Ultities
 {
@@ -6,14 +7,15 @@ namespace NP.WKR.PortOrderBase.Service.Ultities
     {
         private static readonly MemoryCache _cache = new(new MemoryCacheOptions());
 
-        public static void Add(string key, object value, TimeSpan absoluteExpirationRelativeToNow) => _cache.Set(key, value, new MemoryCacheEntryOptions
-        {
-            AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow
-        });
+        public static void Add(PortOrderBaseConfigType key, object value, TimeSpan absoluteExpirationRelativeToNow) =>
+            _cache.Set(key, value, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow
+            });
 
-        public static bool TryGet(string key, out object value) => _cache.TryGetValue(key, out value);
+        public static bool TryGet(PortOrderBaseConfigType key, out object value) => _cache.TryGetValue(key, out value);
 
-        public static void Remove(string key) => _cache.Remove(key);
+        public static void Remove(PortOrderBaseConfigType key) => _cache.Remove(key);
 
         public static void Clear() => _cache.Dispose();
     }
