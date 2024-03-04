25a67f71418a5e9be0b914e5d9a81c4b57674f10
saikn85 <sai.kartik124@gmail.com>
2024-03-04 23:06:22 +0530

    added doc stirng

--- src/NP.WKR.PortOrderBase/GeographicPortWorker.cs
+++ src/NP.WKR.PortOrderBase/GeographicPortWorker.cs
@@ -1,17 +1,26 @@
-namespace NP.WKR.PortOrderBase
-{
-    public class GeographicPortWorker(ILogger<GeographicPortWorker> logger, IConfiguration config) : BackgroundService
-    {
-        private readonly ILogger<GeographicPortWorker> _logger = logger;
-        private readonly IConfiguration _config = config;
+using Microsoft.Extensions.Configuration;
+using NP.WKR.PortOrderBase.Models;
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+namespace NP.WKR.PortOrderBase;
+
+/// <summary>
+/// Worker Service that will read a
+/// </summary>
+/// <param name="logger"></param>
+/// <param name="config"></param>
+public class GeographicPortWorker(ILogger<GeographicPortWorker> logger, IConfiguration config) : BackgroundService
+{
+    private readonly int _defautlSleepInterval = 5;
+    private readonly ILogger<GeographicPortWorker> _logger = logger;
+    private readonly IConfiguration _config = config;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        int sleepInterval = (_config.GetValue<ushort?>("WRK_SleepInterval") ?? _defautlSleepInterval) * 60000;
+        
+        while (!stoppingToken.IsCancellationRequested)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                
-                await Task.Delay(1000, stoppingToken);
-            }
+            await Task.Delay(sleepInterval, stoppingToken);
         }
     }
 }
