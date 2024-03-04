0786c65c4d1d30c4bf516a94f9e8e50dd719ff9e
saikn85 <sai.kartik124@gmail.com>
2024-03-04 23:06:51 +0530

    configured to load env specific settings

--- src/NP.WKR.PortOrderBase/Program.cs
+++ src/NP.WKR.PortOrderBase/Program.cs
@@ -1,10 +1,14 @@
 using NP.WKR.PortOrderBase;
 
 var builder = Host.CreateApplicationBuilder(args);
+var env = builder.Environment;
+
+builder.Configuration
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", false, true);
 
 // Clear ALL Base Provideres
 builder.Logging.ClearProviders();
-
 builder.Services.AddLogging(logBuilder =>
 {
     logBuilder.AddJsonConsole();
