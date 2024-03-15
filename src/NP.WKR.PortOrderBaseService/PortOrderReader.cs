ee187f072f8b1811eb415aa889a880d277eb4f65
Mad Max <sai.kartik124@gmail.com>
2024-03-15 23:33:18 +0530

    updated code

--- src/NP.WKR.PortOrderBaseService/PortOrderReader.cs
+++ src/NP.WKR.PortOrderBaseService/PortOrderReader.cs
@@ -4,10 +4,12 @@ public class PortOrderReader : IPortOrderReader
 {
     public async Task ProcessGeoPortsAsync(CancellationToken token)
     {
+        // Get file path from the cache
+        //       
         try
         {
         }
-        catch (Exception ex)
+        catch (Exception)
         {
         }
     }
